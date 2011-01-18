2b9bfdf6b711376408cc84c76094f348d133f595
Patrick Marschik <patrick.marschik@gmail.com>
2011-01-18 19:51:38 +0100

    basic query/inform implementation

--- src/sos/mas/GamemasterAgent.java
+++ src/sos/mas/GamemasterAgent.java
@@ -1,11 +1,67 @@
 package sos.mas;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.proto.AchieveREInitiator;
+
+import java.util.Date;
+import java.util.Vector;
 
 public class GamemasterAgent extends Agent {
     @Override
     protected void setup() {
         System.out.println("Starting gamemaster agent \"" + getAID().getName());
+
+        Object[] args = getArguments();
+
+        if (args == null || args.length < 3 || args.length > 3) {
+            System.out.println("Need to supply the names of the two prisoner agents and the number of iterations.");
+
+            takeDown();
+        }
+
+        String prisoner1 = (String) args[0];
+        String prisoner2 = (String) args[1];
+        int iterations = Integer.parseInt((String) args[2]);
+
+        ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
+        msg.addReceiver(new AID(prisoner1, AID.ISLOCALNAME));
+        msg.addReceiver(new AID(prisoner2, AID.ISLOCALNAME));
+        msg.setProtocol(FIPANames.InteractionProtocol.FIPA_QUERY);
+
+        msg.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+        msg.setContent("guilty"); // TODO replace by ontology?
+
+        for (int i = 0; i < iterations; i++) {
+            addBehaviour(new AchieveREInitiator(this, msg) {
+                protected void handleFailure(ACLMessage failure) {
+                    if (failure.getSender().equals(myAgent.getAMS())) {
+                        // FAILURE notification from the JADE runtime: the receiver
+                        // does not exist
+                        System.out.println("Responder does not exist");
+                    } else {
+                        System.out.println(
+                                "Agent " + failure.getSender().getName() + " failed to perform the requested action");
+                    }
+                }
+
+                protected void handleAllResultNotifications(Vector notifications) {
+                    for (Object notification : notifications) {
+                        ACLMessage inform = (ACLMessage) notification;
+
+                        // TODO more comprehensive parsing (i.e. handle errors)
+                        boolean complied = inform.getContent().equals("inform-t");
+
+                        // TODO store result
+
+                        System.out.println(
+                                "Agent " + inform.getSender().getName() + " " + (complied ? "complied" : "defected"));
+                    }
+                }
+            });
+        }
     }
 
     @Override
--- src/sos/mas/PrisonerAgent.java
+++ src/sos/mas/PrisonerAgent.java
@@ -1,19 +1,51 @@
 package sos.mas;
 
 import jade.core.Agent;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
 
-/**
- * @author patrick
- */
 public class PrisonerAgent extends Agent {
 
     @Override
     protected void setup() {
-        System.out.println("Starting prisoner agent \"" + getAID().getName());
+        System.out.println("Starting prisoner agent \"" + getLocalName());
+
+        MessageTemplate template = MessageTemplate.and(
+                MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_QUERY),
+                MessageTemplate.MatchPerformative(ACLMessage.QUERY_IF));
+
+        addBehaviour(new AchieveREResponder(this, template) {
+            @Override
+            protected ACLMessage prepareResponse(ACLMessage request) throws NotUnderstoodException, RefuseException {
+                System.out.println("Prisoner " + getLocalName() + ": Agree");
+                ACLMessage agree = request.createReply();
+                agree.setPerformative(ACLMessage.AGREE);
+                return agree;
+            }
+
+            @Override
+            protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response)
+                    throws FailureException {
+                boolean comply = Math.random() > .8;
+
+                System.out.println("Prisoner " + getLocalName() + " " + (comply ? "complies" : "defects"));
+
+                ACLMessage inform = request.createReply();
+                inform.setPerformative(ACLMessage.INFORM);
+                inform.setContent(comply ? "inform-t" : "inform-f");
+
+                return inform;
+            }
+        });
     }
 
     @Override
     protected void takeDown() {
-        System.out.println("Stopping prisoner agent \"" + getAID().getName());
+        System.out.println("Stopping prisoner agent \"" + getLocalName());
     }
 }
