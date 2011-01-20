b4927aff9a3c4b6579721dcb10034e93b2e6d13d
Benjamin Bachhuber <benbachhuber@gmail.com>
2011-01-20 19:32:47 +0100

    First version with ontology

---
+++ src/sos/mas/GameOntology.java
@@ -0,0 +1,21 @@
+package sos.mas;
+import jade.content.onto.BeanOntology;
+import jade.content.onto.*;
+
+
+public class GameOntology extends BeanOntology{
+	private static Ontology theInstance = new GameOntology("GAME_ONTOLOGY");
+	public static Ontology getInstance() {
+		return theInstance;
+	}
+	private GameOntology(String name){
+		super(name);
+		try {
+			add(GameResult.class);
+			add(ResultsIn.class);
+		}
+		catch (Exception e) {
+		e.printStackTrace();
+		}
+	}
+}
